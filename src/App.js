2c79942ff373c512a13bf319e1e904a852754a87
Rupsa Chatterjee <Rupsa.Chatterjee@mjunction.in>
2023-10-09 15:16:11 +0530

    Rupsa first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RPFForm from './pages/RPFForm';
 import RFPEdit from './pages/RFPEdit';
-
+import RFPList from './pages/RFPList';
+import RFPDetailView from './pages/RFPDetailView';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RPFForm />} />
         <Route path="/rfpedit" element={<RFPEdit />} />
+        <Route path="/rfplist" element={<RFPList />} />
+        <Route path="/rfpdetailview/:id" element={<RFPDetailView />} />
+
         {/* Add more routes as needed */}
       </Routes>
     </Router>

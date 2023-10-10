525d8d7bae8c0256a4cd178c7475538abd9e1602
Shashi Nirala <Shashi.Nirala@mjunction.in>
2023-10-10 06:42:22 -0700

    starting bid submmision

--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ window.$ = $;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-      <BidSubmit/>
+    <App />
+    
+      {/* <BidSubmit/> */}
 
     {/* <FileUploadComponent/> */}
 
